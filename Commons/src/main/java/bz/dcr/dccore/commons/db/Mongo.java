8edb4b10a67225bfaeb32a956d06447e8969b2c5
germanelectronix@gmail.com <ac8fb72ed3>
2017-07-24 19:49:43 +0200

    Added mongo-mapper and Mongo class

---
+++ Commons/src/main/java/bz/dcr/dccore/commons/db/Mongo.java
@@ -0,0 +1,46 @@
+package bz.dcr.dccore.commons.db;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+public class Mongo {
+
+    private MongoClientURI uri;
+    private String database;
+
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+
+
+    public Mongo(MongoClientURI uri, String database) {
+        this.uri = uri;
+        this.database = database;
+    }
+
+
+    public void connect() {
+        mongoClient = new MongoClient(uri);
+        mongoDatabase = mongoClient.getDatabase(database);
+    }
+
+    public void disconnect() {
+        if(mongoClient != null) {
+            mongoClient.close();
+        }
+    }
+
+
+    public MongoClientURI getUri() {
+        return uri;
+    }
+
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    public MongoDatabase getMongoDatabase() {
+        return mongoDatabase;
+    }
+
+}
